ba535fa1305c97ef307e9c1712b7eaf67f626c32
Sergej Shafarenka <beworker@gmail.com>
2021-09-11 16:59:24 +0200

    Add timeout for tests

--- src/commonTest/kotlin/utils/utils-coroutines.kt
+++ src/commonTest/kotlin/utils/utils-coroutines.kt
@@ -2,10 +2,13 @@ package de.halfbit.comachine.tests.utils
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.cancelChildren
+import kotlinx.coroutines.withTimeout
 
 fun executeBlockingTest(block: suspend CoroutineScope.() -> Unit) {
     executeBlocking {
-        block()
+        withTimeout(3000) {
+            block()
+        }
         coroutineContext.cancelChildren()
     }
 }
